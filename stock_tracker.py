72f2f94af6bcd437b04ea9d2ccef3cc1fc55ae37
Austin-Buchanan <austinbuchanan233@gmail.com>
2024-04-04 14:47:49 -0700

    Added flag options and a select all method

--- stock_tracker.py
+++ stock_tracker.py
@@ -4,19 +4,37 @@ import psycopg2
 from config import load_config
 
 def stock_tracker():
-    stockTicker = ''
-    stockPrice = ''
-
+    option = ''
     try:
-        stockTicker = sys.argv[1]
-        stockPrice = sys.argv[2]
+        option = sys.argv[1]
     except IndexError:
-        raise SystemExit(f"Usage: {sys.argv[0]} <Stock_Ticker> <Stock_Price>")
-    
-    sqlInsert = """INSERT INTO stock_prices ("stock_ticker", "stock_price", "time_priced") VALUES ('{0}', '{1}', CURRENT_TIMESTAMP) RETURNING price_id;""".format(stockTicker, stockPrice)
-    price_id = None
-    config = load_config()
+        raise SystemExit(f"Usage: {sys.argv[0]} -<Option> [<Stock_Ticker>] [<Stock_Price>]")
 
+    returnCode = None
+    stockTicker = ''
+    match option:
+        case '-i':
+            stockPrice = ''
+            try:
+                stockTicker = sys.argv[2]
+                stockPrice = sys.argv[3]
+                returnCode = add_price(stockTicker, stockPrice)
+            except IndexError:
+                raise SystemExit(f"Usage: {sys.argv[0]} -i <Stock_Ticker> <Stock_Price>")
+        case '-s':
+            if len(sys.argv) > 2:
+                stockTicker = sys.argv[2]
+            returnCode = select_prices(stockTicker)
+        case _:
+            print("Unknown option.")
+
+    if returnCode is not None:
+        print('Operation successful.')    
+
+def add_price(stockTicker, stockPrice):
+    sqlInsert = """INSERT INTO stock_prices ("stock_ticker", "stock_price", "time_priced") VALUES ('{0}', '{1}', CURRENT_TIMESTAMP) RETURNING price_id;""".format(stockTicker, stockPrice)
+    config = load_config()
+    price_id = None
     try:
         with psycopg2.connect(**config) as conn:
             with conn.cursor() as cur:
@@ -24,11 +42,28 @@ def stock_tracker():
                 rows = cur.fetchone()
                 if rows:
                     price_id = rows[0]
-                conn.commit()
+                conn.commit()        
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
-        print(price_id)
+        return price_id
+
+def select_prices(stockTicker):
+    sqlSelect = "SELECT * FROM stock_prices"
+
+    if stockTicker != '':
+        sqlSelect += f" WHERE stock_ticker = '{stockTicker}'"
+
+    config = load_config()
+    try:
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                cur.execute(sqlSelect)
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
 if __name__ == '__main__':
     stock_tracker()
