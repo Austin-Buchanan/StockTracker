64090ce734e63537694b35f88c8348ec45c11b47
Austin-Buchanan <austinbuchanan233@gmail.com>
2024-04-12 13:04:03 -0700

    Added gets for specific ticker queries

--- stock_tracker_api.py
+++ stock_tracker_api.py
@@ -19,6 +19,26 @@ async def get_tickers():
         return {"error": tickers[0]}
     return {"available_tickers": tickers}
 
+@app.get("/stock/{ticker}")
+async def get_recent_price(ticker: str):
+    sqlSelect = """SELECT B.stock_name, A.stock_ticker, A.stock_price, A.time_priced 
+                   FROM stock_prices A inner join stock_names B 
+                   ON A.stock_ticker = B.stock_ticker 
+                   WHERE A.stock_ticker = '{0}' 
+                   ORDER BY A.time_priced DESC 
+                   LIMIT 1;""".format(ticker)
+    results = []
+    success = False
+    success, results = execute_SQL(sqlSelect)
+    if not success:
+        return {"error": results[0]}
+    return {
+       "stock_name": results[0][0],
+       "stock_ticker": results[0][1],
+       "stock_price": results[0][2],
+       "time_priced": results[0][3] 
+    }
+
 def execute_SQL(sql: str):
     config = load_config()
     rows = []
@@ -34,4 +54,7 @@ def execute_SQL(sql: str):
         rows.append[error]
         return False, rows
     finally:
-        return True, rows
+        if len(rows) > 0:
+            return True, rows
+        rows.append("No data found.")
+        return False, rows
